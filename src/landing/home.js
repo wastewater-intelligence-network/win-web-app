3635ffea83fd28609d62b586250284c72ac7b682
TusarHarmanGit <TusharKanta.Mallick@harman.com>
2022-03-08 13:04:36 +0530

    Carousel implement banner section designed

--- src/landing/home.js
+++ src/landing/home.js
@@ -3,75 +3,87 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import WbeTesting from "../Icons/wbe_testing.svg";
 import "./home.css";
-import { Container, Divider } from "@mui/material";
-import Header from "./header";
+import HomeBanner from "../images/banner-image.png";
+import { Button, Container, Divider, Grid } from "@mui/material";
 import WbeWork from "./wbeWorke";
 import WbeWorkDetails from "./wbeWorkDetails";
 import DecisionMakers from "./decisionMakers";
 import CoreComponents from "./coreComponents";
-import Shit from "../Icons/Shit.svg";
 import Footer from "./footer";
 import Partners from "./partners";
-import { textAlign } from "@mui/system";
 import { useEffect, useMemo, useRef } from "react";
 import ScrollButton from "./scrollToTopButton";
 import NewHeader from "./newHeader";
-import HomeHeader from "./home-header";
-
+import BannerLogos from "./bannerLogos";
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 function Home() {
-
   const wbeRef = useRef(null);
 
-
   const OnexecuteScrollWbe = (props) => {
-    console.log(props + " Testing Props")
-    if (props === 'wasteWaterBased') {
+    console.log(props + " Testing Props");
+    if (props === "wasteWaterBased") {
       let _elmScroll = document.getElementById("wasteWaterBasedId");
       _elmScroll.scrollIntoView();
-    }
-    else if (props === 'howWbeWork') {
+    } else if (props === "howWbeWork") {
       let _elmScroll = document.getElementById("howWbeWorkId");
       _elmScroll.scrollIntoView();
-    }
-    else if (props === 'decisionMakers') {
+    } else if (props === "decisionMakers") {
       let _elmScroll = document.getElementById("decisionMakersId");
       _elmScroll.scrollIntoView();
-    }
-    else if (props === 'coreComponents') {
+    } else if (props === "coreComponents") {
       let _elmScroll = document.getElementById("coreComponentsId");
       _elmScroll.scrollIntoView();
-    }
-    else if (props === 'partners') {
+    } else if (props === "partners") {
       let _elmScroll = document.getElementById("partnersId");
       _elmScroll.scrollIntoView();
     }
-
-  }
+  };
 
   return (
     <>
       <NewHeader executeScrollWbe={OnexecuteScrollWbe} />
+      <Box className="banner-section">
+      
+        <img alt="Banner" src={HomeBanner} className="banner-image" />
+        <div className="background-opacity"></div>
+        <Box className="banner-content">
+        <Typography
+          className="banner-text"
+          sx={{ typography: { sm: "h4", xs: "h6" } }}
+        >
+          <span>Wastewater </span> &nbsp; is a reservoir of information about{" "}
+          <span>your health</span> &nbsp; and we mine it to &nbsp;{" "}
+          <span> build insights for the city governments</span>
+        </Typography>
+
+
+<Button className="button_style" sx={{backgroundColor:"#0080C8"}} variant="contained" endIcon={<ArrowRightAltIcon />}>
+How Does it work
+      </Button>
+        <BannerLogos />
+        </Box>
+        {/* <Box className="banner-logos">
+          
+        </Box> */}
+      </Box>
+
       <Container className="mainContainer">
         <Box sx={{ textAlign: "center" }} className="mainBox">
           {/* <Typography
-            className="specingBottomSection larsseitextrabold-font-family headingFontSize"
-            sx={{ typography: { sm: "h3", xs: "h5" } }}
-          >
-            Your <img alt="Shit" src={Shit} /> matters
-          </Typography> */}
-
-          <Typography
             className="customeMarginBottom firstTitle"
-            sx={{ typography: { sm: "h4", xs: "h6" } }}>
+            sx={{ typography: { sm: "h4", xs: "h6" } }}
+          >
             <span>Wastewater Intelligence Network</span>
             is an initiative to bring together experts to manage the spread and
             impact of SARS-CoV-2 through
-            <span> Wastewater-based Epidemiology</span> in
-            Indian cities.
+            <span> Wastewater-based Epidemiology</span> in Indian cities.
           </Typography>
 
-          <Partners />
-          <Divider className="customeMarginTop customeMarginBottom"></Divider>
+          <Partners /> */}
+         
+
+
+
 
           <div id="wasteWaterBasedId">
             <Box className="specingBottomSection">
@@ -79,22 +91,33 @@ function Home() {
                 className="specingBottomSection larsseitextrabold-font-family heading"
                 sx={{ typography: { sm: "h3", xs: "h6" } }}
               >
-                Wastewater-Based Epidemiology
+                How do we mine insights from wastewater?
               </Typography>
 
-              <img
+              <iframe width="100%" height="315" src="https://www.youtube.com/embed/FvPakzqM3h8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+              <video width="320" height="240" controls>
+  <source src="https://www.youtube.com/embed/FvPakzqM3h8" />
+ 
+ </video>
+
+              {/* <img
                 alt="WbeTesting"
                 src={WbeTesting}
                 width="200px"
                 height="166px"
-              />
+              /> */}
               <Typography
                 className="subheading"
                 sx={{ typography: { sm: "h4", xs: "h6" } }}
               >
-                A method of <span>collecting samples of wastewater from sewers, STPs,
-                  lakes etc. and testing these samples for the presence and load of
-                  pathogens </span> like viruses and bacteria. Erstwhile, this method was
+                A method of{" "}
+                <span>
+                  collecting samples of wastewater from sewers, STPs, lakes etc.
+                  and testing these samples for the presence and load of
+                  pathogens{" "}
+                </span>{" "}
+                like viruses and bacteria. Erstwhile, this method was
                 successfully used in India to track Polio and Hepatitis A.
               </Typography>
             </Box>
@@ -105,36 +128,29 @@ function Home() {
           <div id="howWbeWorkId">
             <WbeWork />
           </div>
-
         </Box>
 
-        
         <WbeWorkDetails />
-        
 
         <div id="decisionMakersId">
-        <DecisionMakers />
+          <DecisionMakers />
         </div>
 
         <div id="coreComponentsId">
-        <CoreComponents />
+          <CoreComponents />
         </div>
 
         <div id="partnersId">
-        <Box sx={{ textAlign: "center", my: 6, mx: 6 }}>
-          <Typography
-            className="specingBottomSection heading"
-            sx={{ typography: { sm: "h2", xs: "h4" } }}
-          >
-            Partners
-          </Typography>
-          <Partners />
-        </Box>
+          <Box sx={{ textAlign: "center", my: 6, mx: 6 }}>
+            <Typography
+              className="specingBottomSection heading"
+              sx={{ typography: { sm: "h2", xs: "h4" } }}
+            >
+              Partners
+            </Typography>
+            <Partners />
+          </Box>
         </div>
-
-
-
-
       </Container>
       <Footer />
       <ScrollButton />
--- src/landing/wbeWorkDetails.js
+++ src/landing/wbeWorkDetails.js
@@ -10,66 +10,134 @@ import SampleConcentrationImg from '../Icons/Sample_Concentration.svg';
 import RNAExtraction from '../Icons/RNA_Extraction.svg';
 import RNAMeasurement from '../Icons/RNA_Measurement.svg';
 import DataAnalytics from '../Icons/Data_Analytics.svg';
-
+import "./wbeworkDetailsBScarusel.css";
 import Carousel from 'react-material-ui-carousel'
 import { Paper, Button } from '@mui/material'
+import HomeBanner from "../images/banner-image.png";
 
 
 
-function WbeWorkDetails(props) {
-  const items = [
-    {
-      Name: "Pizza begin",
-      Image: "https://source.unsplash.com/featured/?macbook",
-      contentPosition: "left"
-    },
-    {
-      Name: "Home Appliances",
-      Caption: "Say no to manual home labour!",
-      contentPosition: "middle",
-      Image: "https://source.unsplash.com/featured/?washingmachine"
-    },
-    {
-      Name: "Decoratives",
-      Caption: "Give style and color to your living room!",
-      contentPosition: "right",
-      Image: "https://source.unsplash.com/featured/?lamp"
-    }
-  ];
+// function WbeWorkDetails(props) {
+//   const items = [
+//     {
+//       Name: "Pizza begin",
+//       Image: "https://source.unsplash.com/featured/?macbook",
+//       contentPosition: "left"
+//     },
+//     {
+//       Name: "Home Appliances",
+//       Caption: "Say no to manual home labour!",
+//       contentPosition: "middle",
+//       Image: "https://source.unsplash.com/featured/?washingmachine"
+//     },
+//     {
+//       Name: "Decoratives",
+//       Caption: "Give style and color to your living room!",
+//       contentPosition: "right",
+//       Image: "https://source.unsplash.com/featured/?lamp"
+//     }
+//   ];
 
+//   // return (
+//   //   <Carousel>
+//   //     {
+//   //       items.map((item, i) => <Item key={i} item={item} />)
+//   //     }
+//   //   </Carousel>
+//   // )
+// }
+
+function WbeWorkDetails(){
   return (
-    <Carousel>
-      {
-        items.map((item, i) => <Item key={i} item={item} />)
-      }
-    </Carousel>
+    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+    <div className="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"> Infection</button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2">Shedding</button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3">Sample Collection</button>
+    </div>
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+      <Grid container spacing={2}>
+    <Grid item xs={6} md={7}>
+    <h4>Infection</h4>
+      <p>
+  
+  CoViD-19 is caused by the SARS-CoV-2 virus that spreads through aerosol droplets discharged by an infected person. The incubation period of SARS-CoV-2 has been proven to be of a duration of 2 to 14 days.</p>
+    </Grid>
+    <Grid item xs={6} md={5}>
+    <img src={HomeBanner} className="d-block w-100" alt="..." />
+    </Grid>
+    
+  </Grid>
+  
+      
+       
+      </div>
+      <div className="carousel-item">
+      <Grid container spacing={2}>
+    <Grid item xs={6} md={7}>
+    <h4>Infection</h4>
+      <p>
+  
+  CoViD-19 is caused by the SARS-CoV-2 virus that spreads through aerosol droplets discharged by an infected person. The incubation period of SARS-CoV-2 has been proven to be of a duration of 2 to 14 days.</p>
+    </Grid>
+    <Grid item xs={6} md={5}>
+    <img src={HomeBanner} className="d-block w-100" alt="..." />
+    </Grid>
+    
+  </Grid>
+      </div>
+      <div className="carousel-item">
+      <Grid container spacing={2}>
+    <Grid item xs={6} md={7}>
+    <h4>Infection</h4>
+      <p>
+  
+  CoViD-19 is caused by the SARS-CoV-2 virus that spreads through aerosol droplets discharged by an infected person. The incubation period of SARS-CoV-2 has been proven to be of a duration of 2 to 14 days.</p>
+    </Grid>
+    <Grid item xs={6} md={5}>
+    <img src={HomeBanner} className="d-block w-100" alt="..." />
+    </Grid>
+    
+  </Grid>
+      </div>
+    </div>
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      {/* <span className="visually-hidden">Previous</span> */}
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      {/* <span className="visually-hidden">Next</span> */}
+    </button>
+  </div>
   )
 }
 
 export default WbeWorkDetails;
 
+// function Item(props) {
+   
 
-function Item(props) {
-  return (
-      <Grid
-        container
-        rowSpacing={3}
-        columnSpacing={{ xs: 3, sm: 3, md: 12 }}
-        justifyContent="center"
-        alignItems="center">
-        <Grid item xs={12} sm={6} md={6} >
-          {props.item.Caption}
-          img elements must have an alt prop, either with meaningful text, or an empty string fo
-          img elements must have an alt prop, either with meaningful text, or an empty string fo
-          img elements must have an alt prop, either with meaningful text, or an empty string fo
-          img elements must have an alt prop, either with meaningful text, or an empty string fo
-          img elements must have an alt prop, either with meaningful text, or an empty string fo
+//       <Grid
+//         container
+//         rowSpacing={3}
+//         columnSpacing={{ xs: 3, sm: 3, md: 12 }}
+//         justifyContent="center"
+//         alignItems="center">
+//         <Grid item xs={12} sm={6} md={6} >
+//           {props.item.Caption}
+//           img elements must have an alt prop, either with meaningful text, or an empty string fo
+//           img elements must have an alt prop, either with meaningful text, or an empty string fo
+//           img elements must have an alt prop, either with meaningful text, or an empty string fo
+//           img elements must have an alt prop, either with meaningful text, or an empty string fo
+//           img elements must have an alt prop, either with meaningful text, or an empty string fo
 
-        </Grid>
+//         </Grid>
         
-          <Grid item xs={12} sm={6} md={6} >
-            <img src={props.item.Image} style={{ width: 100 + '%' }} />
-          </Grid>
-        </Grid>
-  )
-}
+//           <Grid item xs={12} sm={6} md={6} >
+//             <img src={props.item.Image} style={{ width: 100 + '%' }} />
+//           </Grid>
+//         </Grid>
+//   )
+// }
