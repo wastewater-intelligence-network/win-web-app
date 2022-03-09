b9f3e1153cf23dabc5f90d0919d01dac19f1d693
lokeshharman <lokesh.pratap@harman.com>
2022-03-09 14:01:56 +0530

    Added Slick-React package for Partners Section multi Logo Carosuel and custom styling for arrows

--- src/landing/partners.js
+++ src/landing/partners.js
@@ -6,13 +6,58 @@ import Mohua from "../Icons/mohua.svg";
 import Smc from "../Icons/smc.svg";
 import Sscdl from "../Icons/sscdl.svg";
 import { Divider, Grid, Typography } from "@mui/material";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./partners.css";
+
+
 function Partners() {
- 
+
+  let imageCount = 5;
+  var settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    arrows: imageCount > 7 ? true : false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true
+        }
+      }
+    ]
+  };
 
   return (
     <>
 
-
+      {/* 
       <Box className="specingBottomSection" sx={{ width: "100%", my: 4 }}>
         <Grid
           container
@@ -38,9 +83,26 @@ function Partners() {
           </Grid>
           
         </Grid>
-      </Box>
+      </Box> */}
 
-      
+
+      <Slider {...settings}>
+        <div>
+          <img style={{ maxWidth: "100%", width: '85px' }} alt="Iscf" src={Iscf} />
+        </div>
+        <div>
+          <img style={{ maxWidth: "100%", width: '192px' }} alt="Nius" src={Niua} />
+        </div>
+        <div>
+          <img style={{ maxWidth: "100%", width: '194px' }} alt="Mohua" src={Mohua} />
+        </div>
+        <div>
+          <img style={{ maxWidth: "100%", width: ' 95px' }} alt="Smc" src={Smc} />
+        </div>
+        <div>
+          <img style={{ maxWidth: "100%", width: '221px' }} alt="Sscdl" src={Sscdl} />
+        </div>
+      </Slider>
     </>
   );
 }
